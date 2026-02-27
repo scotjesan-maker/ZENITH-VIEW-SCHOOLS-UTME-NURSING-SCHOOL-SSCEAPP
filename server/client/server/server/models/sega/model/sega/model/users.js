c50b96a24a86f5221af3b3109dc012484da94a28
scotjesan-maker <scotjesan@gmail.com>
2026-02-27 21:22:44 +0100

    Add users.js model

---
+++ server/client/server/server/models/sega/model/sega/model/users.js
@@ -0,0 +1,12 @@
+// sega/model/users.js
+const users = [
+  {
+    id: 1,
+    name: "Admin",
+    email: "admin@example.com",
+    password: "admin123", // in real apps, use hashing
+    role: "admin"
+  }
+];
+
+module.exports = users;
