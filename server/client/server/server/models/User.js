ffafb8e9d555d031b528acdf49a6cef5b43215be
scotjesan-maker <scotjesan@gmail.com>
2026-02-27 21:07:39 +0100

    server/models/

---
+++ server/client/server/server/models/User.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  username: String,
+  email: String,
+  password: String,
+  score: { type: Number, default: 0 }
+});
+
+module.exports = mongoose.model("User", UserSchema);
