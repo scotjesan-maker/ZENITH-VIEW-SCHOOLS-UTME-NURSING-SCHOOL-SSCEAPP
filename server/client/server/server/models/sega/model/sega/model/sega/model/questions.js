0ef42c856da103abc4df404c6ed8e1bae1b608e4
scotjesan-maker <scotjesan@gmail.com>
2026-02-27 21:25:24 +0100

    Add initial question bank

---
+++ server/client/server/server/models/sega/model/sega/model/sega/model/questions.js
@@ -0,0 +1,20 @@
+// sega/model/questions.js
+const questions = [
+  {
+    id: 1,
+    subject: "Mathematics",
+    question: "What is 2 + 2?",
+    options: ["1", "2", "3", "4"],
+    answer: "4"
+  },
+  {
+    id: 2,
+    subject: "English",
+    question: "Which is a noun?",
+    options: ["Run", "Happiness", "Quickly", "Blue"],
+    answer: "Happiness"
+  }
+  // continue adding questions...
+];
+
+module.exports = questions;
